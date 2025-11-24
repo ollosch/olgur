4f8b4b0abd9c0d1a953a6c6fd65e24d813a2998d
birusch <15702230+birusch@users.noreply.github.com>
2025-11-24 17:04:21 +0100

    test(api/auth) User registration

---
+++ backend/tests/Feature/Auth/RegistrationTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+test('new users can register', function () {
+    $this->withoutExceptionHandling();
+
+    $credentials = [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ];
+
+    $response = $this->post(route('register'), $credentials);
+
+    $response->assertNoContent();
+    $this->assertDatabaseHas('users', [
+        'name' => $credentials['name'],
+        'email' => $credentials['email'],
+    ]);
+});
