826c09ee1e3377a266f9e27fe92ec4237399642d
birusch <15702230+birusch@users.noreply.github.com>
2025-11-24 16:58:58 +0100

    chore(api/db) Seeder creates a single user

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 final class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        User::factory()->create([
+            'name' => 'Ollo',
+            'email' => 'ollo@olwu.de',
+        ]);
     }
 }
