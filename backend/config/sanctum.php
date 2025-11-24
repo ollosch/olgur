d9c632877d9afea55cf01a96fb45f80cf30a85d2
birusch <15702230+birusch@users.noreply.github.com>
2025-11-24 16:57:29 +0100

    conf(api/auth) Configure Sanctum for token-only use

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -1,7 +1,5 @@
 <?php
 
-use Laravel\Sanctum\Sanctum;
-
 return [
 
     /*
@@ -15,12 +13,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
-    ))),
+    'stateful' => [],
 
     /*
     |--------------------------------------------------------------------------
@@ -34,7 +27,7 @@ return [
     |
     */
 
-    'guard' => ['web'],
+    'guard' => [],
 
     /*
     |--------------------------------------------------------------------------
@@ -75,10 +68,5 @@ return [
     |
     */
 
-    'middleware' => [
-        'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
-        'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
-    ],
-
+    'middleware' => [],
 ];
