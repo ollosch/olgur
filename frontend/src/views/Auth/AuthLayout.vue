6701561db0191ab3c15472071ccebe38bc75b35e
birusch <15702230+birusch@users.noreply.github.com>
2025-11-25 14:47:22 +0100

    feat(ui/auth) Create views for Login and Registration

---
+++ frontend/src/views/Auth/AuthLayout.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <div class="w-full max-w-lg mx-auto">
+      <slot></slot>
+    </div>
+  </div>
+</template>
---
+++ frontend/src/views/Auth/LoginView.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Field, FieldDescription, FieldGroup, FieldLabel } from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+
+import AuthLayout from './AuthLayout.vue'
+</script>
+
+<template>
+  <AuthLayout>
+    <Card>
+      <CardHeader>
+        <CardTitle>Login to Olwu</CardTitle>
+        <CardDescription>
+          Enter your email and password below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <FieldGroup>
+            <Field>
+              <FieldLabel for="email"> Email </FieldLabel>
+              <Input id="email" type="email" placeholder="m@example.com" required />
+            </Field>
+            <Field>
+              <div class="flex items-center">
+                <FieldLabel for="password"> Password </FieldLabel>
+                <a href="#" class="ml-auto inline-block text-sm underline-offset-4 hover:underline">
+                  Forgot your password?
+                </a>
+              </div>
+              <Input id="password" type="password" required />
+            </Field>
+            <Field>
+              <Button type="submit"> Login </Button>
+              <FieldDescription class="text-center">
+                Don't have an account?
+                <router-link :to="{ name: 'register' }"> Sign up </router-link>
+              </FieldDescription>
+            </Field>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  </AuthLayout>
+</template>
