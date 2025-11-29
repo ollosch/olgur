f6afdf5197e4e417dd93c87c9fd0cc9f39972337
birusch <15702230+birusch@users.noreply.github.com>
2025-11-29 16:00:21 +0100

    feat(ui/auth) Add authentication guard to router

--- frontend/src/router/auth.ts
+++ frontend/src/router/auth.ts
@@ -4,11 +4,13 @@ export default [
   {
     path: '/login',
     name: 'login',
+    meta: { requiresGuest: true },
     component: LoginView,
   },
   {
     path: '/register',
     name: 'register',
+    meta: { requiresGuest: true },
     component: () => import('@/views/Auth/RegisterView.vue'),
   },
 ]
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import authRoutes from '@/router/auth.ts'
+import { useAuthStore } from '@/stores/auth.ts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,9 +9,50 @@ const router = createRouter({
     {
       path: '/dashboard',
       name: 'dashboard',
+      meta: { requiresAuth: true },
       component: () => import('@/views/DashboardView.vue'),
     },
   ],
+})
+
+/**
+ * Navigation Guard
+ *
+ * @remarks
+ * This guard checks the user's authentication status before each route change.
+ * It ensures that:
+ * - Routes requiring authentication are only accessible to authenticated users.
+ * - Routes requiring guest access are only accessible to unauthenticated users.
+ * - If the user's auth token is present but their session is not validated,
+ *   it attempts to validate the session by calling `checkAuth`.
+ *
+ * If a user tries to access a protected route without being authenticated,
+ * they are redirected to the login page with an optional `redirect` query
+ * parameter indicating their original destination. Conversely, authenticated
+ * users trying to access guest-only routes are redirected to the dashboard.
+ */
+router.beforeEach(async (to) => {
+  const auth = useAuthStore()
+
+  if (!auth.isAuthenticated && auth.token) {
+    const isValid = await auth.check()
+    if (!isValid) {
+      auth.clear()
+    }
+  }
+
+  const isAuthenticated = auth.isAuthenticated
+  const requiresAuth = to.meta.requiresAuth
+  const requiresGuest = to.meta.requiresGuest
+
+  if (requiresAuth && !isAuthenticated) {
+    return {
+      name: 'login',
+      query: { redirect: to.fullPath },
+    }
+  } else if (requiresGuest && isAuthenticated) {
+    return { name: 'dashboard' }
+  }
 })
 
 export default router
