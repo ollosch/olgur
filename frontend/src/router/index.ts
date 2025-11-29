2b7bc45957d461dc80f84fcbe0758a096d9bb8cc
birusch <15702230+birusch@users.noreply.github.com>
2025-11-29 15:58:15 +0100

    feat(ui) Add AppLayout and Dashboard skeletons

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,7 +3,14 @@ import authRoutes from '@/router/auth.ts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [...authRoutes],
+  routes: [
+    ...authRoutes,
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('@/views/DashboardView.vue'),
+    },
+  ],
 })
 
 export default router
---
+++ frontend/src/views/DashboardView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AppLayout from './AppLayout.vue'
+</script>
+
+<template>
+  <AppLayout>
+    <h1 class="text-2xl font-bold mb-4">Dashboard</h1>
+  </AppLayout>
+</template>
