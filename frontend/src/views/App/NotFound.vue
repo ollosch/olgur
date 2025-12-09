1e95c9ee5b5864eb096192ea0374e9b29034c685
birusch <15702230+birusch@users.noreply.github.com>
2025-12-09 10:06:06 +0100

    fix(ui/404) Hide "back" link if there is no "back"

---
+++ frontend/src/views/App/NotFound.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import AuthLayout from '@/views/Auth/AuthLayout.vue'
+
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
+
+const historyNotEmpty = window.history.state?.back !== null
+</script>
+
+<template>
+  <AuthLayout>
+    <Card>
+      <CardHeader>
+        <CardTitle>404 - Not Found</CardTitle>
+        <CardDescription> The page you are looking for does not exist. </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <span v-if="historyNotEmpty">
+          <button @click="$router.back()" class="text-blue-500 underline mr-2 cursor-pointer">
+            Go back</button
+          >, or
+        </span>
+        <router-link :to="{ name: 'rules' }" :replace="true" class="text-blue-500 underline"
+          >Go home</router-link
+        >
+      </CardContent>
+    </Card>
+  </AuthLayout>
+</template>
